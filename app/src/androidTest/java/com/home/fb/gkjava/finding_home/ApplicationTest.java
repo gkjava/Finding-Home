2522f40922139bd2038bb04402b9d69eaa244dd3
Gaurav <gkjva@outlook.com>
2016-08-27 23:51:57 +0530

    Initial commit

---
+++ app/src/androidTest/java/com/home/fb/gkjava/finding_home/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.home.fb.gkjava.finding_home;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
